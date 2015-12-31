2828343f47fd228b0940cb14b670f9422e4f65d6
Helton Wernik <Wernik@macbook-pro-de-israel.local>
2015-12-31 17:45:59 -0200

    Initial Commit

---
+++ jogo da velha monica/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jogo da velha monica
+//
+//  Created by Helton Wernik on 31/12/15.
+//  Copyright © 2015 Connecta Rocks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
